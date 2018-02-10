3d60299c6d32910a309104d4499a2b51f0590f3c
Suyash Kumar <suyashkumar2003@gmail.com>
2018-02-10 00:11:15 -0500

    Implement better permissioning (#3)

--- user.go
+++ user.go
@@ -9,9 +9,11 @@ import (
 
 type Permissions int64
 
+// These permission levels are ORDERED lowest to highest
 const (
-	PERMISSIONS_USER  = 0
-	PERMISSIONS_ADMIN = 1
+	PERMISSIONS_API   = 0
+	PERMISSIONS_USER  = 1
+	PERMISSIONS_ADMIN = 2
 )
 
 func (p *Permissions) Scan(value interface{}) error {
@@ -25,13 +27,13 @@ func (p Permissions) Value() (driver.Value, error) {
 
 // User represents a generic User that Suyash expects in his projects. TODO: generalize
 type User struct {
-	UUID           uuid.UUID `sql:"type:uuid;"`
-	Email          string    `gorm:"unique_index"`
-	HashedPassword string
-	FirstName      string
-	LastName       string
-	Permissions    Permissions
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
-	DeletedAt      time.Time `sql:"default:NULL"`
+	UUID               uuid.UUID `sql:"type:uuid;"`
+	Email              string    `gorm:"unique_index"`
+	HashedPassword     string
+	FirstName          string
+	LastName           string
+	MaxPermissionLevel Permissions
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
+	DeletedAt          time.Time `sql:"default:NULL"`
 }
